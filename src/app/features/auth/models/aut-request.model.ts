c5e570a7c10c3583f705ddd6d86ae377ab03417f
Angy <angynataliamejia@gmail.com>
2025-09-23 18:20:07 -0500

    Rename modules folder to features

---
+++ src/app/features/auth/models/aut-request.model.ts
@@ -0,0 +1,19 @@
+import { User } from './user.model';
+
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+
+export interface RegisterRequest {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export interface AuthResponse {
+    user: User;
+    token: string;
+    success: boolean;
+    message: string;
+}
