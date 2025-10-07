0c9ebb9bc95b45a9c69fd06356fb55d95a9e95a6
Angy <angynataliamejia@gmail.com>
2025-10-07 14:16:20 -0500

    Api change and auth module

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api',
+    apiUrl: 'http://localhost:3000/api/v1',
     endpoints: {
         auth: {
             login: '/auth/login',
